3e9d670c88a9af01d2156a84fdb1b3dd137ce73c
Ryan Wittman <OfficiallyRy@outlook.com>
2023-03-01 11:38:37 -0700

    Wrote out like what... every back end event function?

--- Tower/server/controllers/EventsController.js
+++ Tower/server/controllers/EventsController.js
@@ -1,3 +1,4 @@
+import { Auth0Provider } from "@bcwdev/auth0provider";
 import { eventsService } from "../services/EventsService.js";
 import BaseController from "../utils/BaseController.js";
 
@@ -6,15 +7,63 @@ export class EventsController extends BaseController{
   constructor(){
     super('api/events')
     this.router
+    .get('', this.getAllEvents)
+    .get('/:eventId', this.getEventById)
+    .use(Auth0Provider.getAuthorizedUserInfo)
     .post('', this.createEvent)
+    .put('/:eventId', this.editEvent)
+    .delete('/:eventId', this.cancelEvent)
 
   }
 
   async createEvent(req, res, next){
     try {
+      const user = req.userInfo
       const eventData = req.body
+      eventData.creatorId = user.id
       const event = await eventsService.createEvent(eventData)
       res.send(event)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getAllEvents(req, res, next){
+    try {
+      const events = await eventsService.getAllEvents()
+      res.send(events)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getEventById(req, res, next){
+    try {
+      const eventId = req.params.eventId
+      const event = await eventsService.getEventById(eventId)
+      res.send(event)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async editEvent(req, res, next){
+    try {
+      const requestorId = req.userInfo.id
+      const eventData = req.body
+      const editedEvent = await eventsService.editEvent(eventData, requestorId)
+      res.send(editedEvent)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async cancelEvent(req, res, next){
+    try {
+      const requestorId = req.userInfo.id
+      const eventId = req.params.eventId
+      const canceledEvent = await eventsService.cancelEvent(eventId, requestorId)
+      res.send(canceledEvent)
     } catch (error) {
       next(error)
     }
--- Tower/server/services/EventsService.js
+++ Tower/server/services/EventsService.js
@@ -1,12 +1,46 @@
 import { dbContext } from "../db/DbContext.js"
+import { Forbidden } from "../utils/Errors.js"
 
 
 class EventsService{
   async createEvent(eventData) {
-    let event = dbContext.Events.create(eventData)
+    let event = await dbContext.Events.create(eventData)
+    return event
+  }
+  
+  async getAllEvents(){
+    let events = await dbContext.Events.find()
+    .populate('creator', 'name picture')
+    return events
+  }
+  
+  async getEventById(eventId) {
+    let event = await dbContext.Events.findById(eventId)
+    .populate('creator', 'name picture')
+    return event
+  }
+  async editEvent(eventData, requestorId) {
+    let event = await dbContext.Events.findById(eventData.id)
+    if(requestorId != event.creatorId){
+      throw new Forbidden("That's not your event, silly!")
+    }
+    event.name = eventData.name || event.name
+    event.description = eventData.description || event.description
+    event.capacity = eventData.capacity || event.capacity
+    event.type = eventData.type || event.type
+    event.startDate = eventData.startDate || event.startDate
+    event.coverImg = eventData.coverImg || event.coverImg
     return event
   }
 
+  async cancelEvent(eventId, requestorId){
+    let event = await dbContext.Events.findById(eventId)
+    if(requestorId != event.creatorId){
+      throw new Forbidden("That's not your event, silly!")
+    }
+    event.isCanceled = true
+    return event
+  }
 }
 
 export const eventsService = new EventsService()
