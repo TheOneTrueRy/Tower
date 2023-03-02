d06001ed7459e0975d832189f5639466eeaa53e4
Ryan Wittman <OfficiallyRy@outlook.com>
2023-03-02 09:23:28 -0700

    All events now drawing to page and styled nicely

--- Tower.client/src/pages/HomePage.vue
+++ Tower.client/src/pages/HomePage.vue
@@ -9,13 +9,15 @@
 
         </div>
       </div>
-      <div class="col-3 p-2 mt-3 event">
-        <div class="event-top"></div>
-        <div class="event-bottom p-1 text-light d-flex flex-column justify-content-end">
-          <span><b>Event Name</b></span>
-          <span>Event Location</span>
-          <span>Event Date</span>
-          <span class="text-end"><b>200</b> Spots Left</span>
+      <div v-for="e in events" class="col-3 p-2 mt-3 event">
+        <div :style="{ backgroundImage: `url(${e.coverImg})` }" class="h-100 w-100 event-bg">
+          <div class="event-top"></div>
+          <div class="event-bottom p-1 text-light d-flex flex-column justify-content-end my-shadow">
+            <span><b>{{ e.name }}</b></span>
+            <span>{{ e.location }}</span>
+            <span>{{ e.date }}</span>
+            <span class="text-end"><b>{{ e.capacity }}</b> Spots Left</span>
+          </div>
         </div>
       </div>
     </div>
@@ -23,12 +25,35 @@
 </template>
 
 <script>
+import { computed, ref, onMounted } from "vue";
+import { AppState } from "../AppState.js";
+import { eventsService } from "../services/EventsService.js";
+import Pop from "../utils/Pop.js";
+
 export default {
   setup() {
+    const filterType = ref('')
+    async function getAllEvents() {
+      try {
+        await eventsService.getAllEvents()
+      } catch (error) {
+        Pop.error(error.message, 'Getting Events')
+      }
+    }
 
+    onMounted(() => {
+      getAllEvents()
+    })
 
     return {
-
+      events: computed(() => {
+        if (!filterType.value) {
+          return AppState.events
+        } else {
+          return AppState.events.filter(e => e.type == filterType.value)
+        }
+      }),
+      account: computed(() => AppState.account)
     }
   }
 }
@@ -37,7 +62,10 @@ export default {
 <style scoped lang="scss">
 .event {
   height: 300px;
-  width: 300px;
+}
+
+.event-bg {
+  background-size: cover;
 }
 
 .event-top {
@@ -47,5 +75,9 @@ export default {
 .event-bottom {
   height: 35%;
   background-color: rgba(255, 255, 255, 0.203);
+}
+
+.my-shadow {
+  text-shadow: 0 0 4px black;
 }
 </style>
--- Tower.client/src/services/EventsService.js
+++ Tower.client/src/services/EventsService.js
@@ -1,5 +1,6 @@
 import { AppState } from "../AppState.js"
 import { Event } from "../models/Event.js"
+import { api } from "./AxiosService.js"
 
 
 class EventsService{
@@ -7,6 +8,11 @@ class EventsService{
   async createEvent(eventData){
     const res = await api.post('api/events', eventData)
     AppState.events.unshift(new Event(res.data))
+  }
+
+  async getAllEvents(){
+    const res = await api.get('api/events')
+    AppState.events = res.data.map(e => new Event(e))
   }
 
 }
