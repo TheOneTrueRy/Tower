f94c80d394432032b496b8926ee98d66a6c972a8
Ryan Wittman <OfficiallyRy@outlook.com>
2023-03-17 14:51:15 -0600

    changed auth0 application for Tower

--- Tower.client/src/env.js
+++ Tower.client/src/env.js
@@ -2,5 +2,5 @@ export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
 export const domain = 'theonetruery.us.auth0.com'
-export const clientId = 'akV13tVJ4Z0r96mr8BwmKaFGah40DVxm'
+export const clientId = 'igDnxBuQAnleFr9VOKP33ov5UNEvt1rZ'
 export const audience = 'https://big-bango-bongo.com'
